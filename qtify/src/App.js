f36395854d92be0f6cc4f3345b2447bf71e06914
ShawnFrost26 <sb262202@gmail.com>
2023-09-14 11:37:26 +0530

    card-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,12 +1,35 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { fetchTopAlbums } from "./api/api";
 import Hero from "./components/Hero/Hero";
 import NavBar from "./components/NavBar/NavBar";
+import Card from "./components/Card/Card";
 
 function App() {
+  const [topAlbumData, setTopAlbumData] = useState([]);
+
+  const generateTopAlbumData = async () => {
+    try {
+      const data = await fetchTopAlbums();
+      setTopAlbumData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    generateTopAlbumData();
+  }, []);
+
   return (
     <div className="App">
       <NavBar />
       <Hero />
+      <div className="card-container">
+        {topAlbumData.map((item) => {
+          return <Card key={item.id} data={item} type="album" />;
+        })}
+      </div>
     </div>
   );
 }
